b3165c23ed908129292706071ec9614da5e9bab6
Minhnqph40837 <minhdaonho@gmail.com>
2025-01-18 07:43:22 +0700

    fix vv

--- src/Data/Movies.ts
+++ src/Data/Movies.ts
@@ -30,20 +30,20 @@ const movies: Movie[] = [
     rating: 4.9,
     category: "Anime",
     imgSrc: "/images/one-punch-man-season-1.jpg", // Không cần /public
-    trailer: "https://youtu.be/1hz-sFiqdks?si=lHg6-scQfq7WooZl",
+    trailer: "/video_trailer/one_punch_man_s3_trailer.mp4", // Không cần /public
     desc: "One Punch Man kể về Saitama, một anh hùng có thể đánh bại bất kỳ kẻ thù nào chỉ với một cú đấm.",
     episodes: [
       {
         id: "1",
         name: "Tập 1: Thực thi chính nghĩa",
-        video: "https://youtu.be/1hz-sFiqdks?si=lHg6-scQfq7WooZl",
+        video: "/video_trailer/one_punch_man_s3_trailer.mp4", // Không cần /public
         duration: "24 phút",
         poster: "/poster/ss1_tap1.jpg", // Không cần /public
       },
       {
         id: "2",
         name: "Tập 2: Chàng Cyborg cô độc",
-        video: "https://youtu.be/1hz-sFiqdks?si=lHg6-scQfq7WooZl",
+        video: "/video_trailer/one_punch_man_s3_trailer.mp4",
         duration: "24 phút",
         poster: "/poster/ss1_tap2.jpg", // Không cần /public
       },
